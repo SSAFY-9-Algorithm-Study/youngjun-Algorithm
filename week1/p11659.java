ef63f73714da2f08816395554f8bd3effe92deb2
leeyjwinter <james_22@naver.com>
2023-02-04 21:20:00 +0900

    first commit

---
+++ week1/p11659.java
@@ -0,0 +1,31 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+//https://www.acmicpc.net/problem/11659 구간합 구하기
+public class p11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int nums = Integer.parseInt(st.nextToken());
+        int sections = Integer.parseInt(st.nextToken());
+        int numArray[] = new int[nums+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= nums; i++) {
+            numArray[i] = numArray[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < sections; i++) {
+            st = new StringTokenizer(br.readLine());
+            int sectionStart = Integer.parseInt(st.nextToken());
+            int sectionEnd = Integer.parseInt(st.nextToken());
+            System.out.println(numArray[sectionEnd] - numArray[sectionStart-1]);
+        }
+    }
+}
