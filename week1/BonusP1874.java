ccfe378ea2452201287c1640154d29be0ffd8e2d
leeyjwinter <james_22@naver.com>
2023-02-07 21:24:42 +0900

    p1874(Stack) : Upload Answer

--- week1/BonusP1874.java
+++ week1/BonusP1874.java
@@ -1,4 +1,46 @@
 package week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class BonusP1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int nums = Integer.parseInt(bf.readLine());
+        int cur = 1;
+        Stack<Integer> numStack = new Stack<>();
+        List<String> ans = new ArrayList<>();
+
+
+        for (int i = 0; i < nums; i++) {
+            int num = Integer.parseInt(bf.readLine());
+            if(cur>num){
+                if(num!=numStack.pop()){
+                    ans.clear();
+                    ans.add("NO");
+                    break;
+                }
+                else{
+                    ans.add("-");
+                }
+            }
+            else{
+                for (int j = cur; j <= num; j++) {
+                    numStack.add(j);
+                    ans.add("+");
+                }
+                cur = numStack.pop()+1;
+                ans.add("-");
+            }
+
+        }
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.println(ans.get(i));
+        }
+    }
 }
