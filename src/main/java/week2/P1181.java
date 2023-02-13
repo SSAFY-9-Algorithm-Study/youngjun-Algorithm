55668d6c91ce422f92c3008078fecbd01c854055
leeyjwinter <james_22@naver.com>
2023-02-13 23:31:00 +0900

    upload week2

---
+++ src/main/java/week2/P1181.java
@@ -0,0 +1,37 @@
+package week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class P1181 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		String[] words = new String[n];
+		for (int i = 0; i < n; i++) {
+			words[i] = bf.readLine();
+		}
+		Arrays.sort(words);
+		Arrays.sort(words, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				
+				return o1.length() - o2.length();
+			}
+		});
+		
+		for (int i = 0; i < words.length; i++) {
+			if(i<words.length-1 && !words[i].equals(words[i+1]))
+			System.out.println(words[i]);
+		}
+		System.out.println(words[words.length-1]);
+	}
+
+}
---
+++ src/main/java/week2/P18870.java
@@ -0,0 +1,61 @@
+package week2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class P18870 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(bf.readLine());
+		int arr[] = new int[n];
+		int orderedArr[] = new int[n];
+		Map<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			
+		}
+		
+		
+		System.arraycopy(arr, 0, orderedArr, 0, n);
+		Arrays.sort(orderedArr);
+		int rank = 0;
+		
+		for (int key : orderedArr) {
+			if(!map.containsKey(key)) {
+				map.put(key, rank++);
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		 for (int key : arr) {
+			 sb.append(map.get(key)).append(' ');
+		}
+		 System.out.println(sb);
+
+	}
+
+}
+
+
+//죽어라 자바
+//for (int i = 0; i < n-1; i++) {
+//	if(orderedArr[i]!=orderedArr[i+1])
+//		undupArr.add(orderedArr[i]);
+//}
+//undupArr.add(orderedArr[n-1]);
+//
+//for (int i = 0; i < n; i++) {
+//	for (int j = 0; j < undupArr.size(); j++) {
+//		if(arr[i] == undupArr.get(j)) {
+//			System.out.print(j + " ");
+//			break;
+//		}
+//	}
+//}
