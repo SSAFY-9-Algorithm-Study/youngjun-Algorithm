535c92e2ba946081762337e3daeee2908a14b115
leeyjwinter <james_22@naver.com>
2023-02-22 09:26:23 +0900

    0221 : 14889 DFS

--- src/main/java/week3/BonusP14889.java
+++ src/main/java/week3/BonusP14889.java
@@ -9,42 +9,68 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class BonusP14889 {
-	
+	static int n;
+	static int nums[];
+	static boolean[] visit;
+	static int mat[][];
+	static int min = Integer.MAX_VALUE;;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int nums[] = new int[n];
-		List<int[]> comb = new ArrayList<>();
-		int[] list = new int[2];
-		int [][] mat = new int[n][n];
+		n = Integer.parseInt(br.readLine());
+		nums = new int[n];
+		visit = new boolean[n];
+		mat = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			nums[i] = i;
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				mat[i][j] = Integer.parseInt(st.nextToken());
 			}
-			
 		}
-		DFS(0,0,n,comb,list,nums);
-		
+		DFS(0, 0);
+
+		System.out.println(min);
+
 	}
-	
-	public static void DFS(int L, int begin, int n, List<int[]> comb,  int[] list, int[] nums) {
-		
-		if(L==2) {
-			System.out.println(Arrays.toString(list));
-			comb.add(list);
+
+	public static void DFS(int L, int begin) {
+		if (L == n / 2) {
+			diff();
+			return;
 		}
-		else {
-			for (int i = begin; i < n; i++) {
-				list[L] = nums[i];
-				DFS(L+1,i+1,n,comb,list,nums);
+		for (int i = begin; i < n; i++) {
+			visit[i] = true;
+			DFS(L + 1, i + 1);
+			visit[i] = false;
+		}
+	}
+
+	public static void diff() {
+		int start = 0;
+		int link = 0;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (visit[i] == true && visit[j] == true) {
+					start += mat[i][j];
+					start += mat[j][i];
+				} else if (visit[i] == false && visit[j] == false) {
+					link += mat[i][j];
+					link += mat[j][i];
+				}
+
 			}
 		}
-		
-		
-		
+
+		int val = Math.abs(start - link);
+
+		if (val == 0) {
+			System.out.println(val);
+			System.exit(0);
+		}
+
+		min = Math.min(min, val);
+
 	}
-	
 
 }
