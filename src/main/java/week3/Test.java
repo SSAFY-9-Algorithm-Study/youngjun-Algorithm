ace8d482df5e96ba393433c78ea32b7d0f4531a7
leeyjwinter <james_22@naver.com>
2023-02-23 17:53:50 +0900

    Revert "0223 : Practice DFS"
    This reverts commit 19bb9567ca936af3a8d6ff0406ca1637341ea79c.

--- src/main/java/week3/Test.java
+++ src/main/java/week3/Test.java
@@ -1,7 +1,5 @@
 package week3;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,25 +9,19 @@ public class Test {
 	public static void main(String[] args) {
 		TreeMap<Integer, String> tMap = new TreeMap<Integer, String>();
 		Map<Integer, String> map = new HashMap<Integer, String>();
-		Deque<Integer> deq = new ArrayDeque<>();
-//		tMap.put(5, "fff");
-//		tMap.put(3, "bbb");
-//		tMap.put(2, "ccc");
-//		tMap.put(7, "ddd");
-//		tMap.put(1, "aaa");
-//		tMap.put(1, "cccc");
-//		
-//		String a = tMap.replace(2, "ddd");
-//		System.out.println(a);
-//		System.out.println(tMap);
-//		System.out.println(tMap.firstEntry());
-//		System.out.println(tMap.ceilingEntry(6)); //6 이사큰 키 중 가장 작은 키
-//		
-//		System.out.println((int)'0');
-//		
-//		StringBuffer sb = new StringBuffer("abc");
-//		String rev = sb.reverse().toString();
-//		System.out.println(rev);
+		
+		tMap.put(5, "fff");
+		tMap.put(3, "bbb");
+		tMap.put(2, "ccc");
+		tMap.put(7, "ddd");
+		tMap.put(1, "aaa");
+		tMap.put(1, "cccc");
+		
+		String a = tMap.replace(2, "ddd");
+		System.out.println(a);
+		System.out.println(tMap);
+		System.out.println(tMap.firstEntry());
+		System.out.println(tMap.ceilingEntry(6)); //6 이사큰 키 중 가장 작은 키
 	}
 
 }
