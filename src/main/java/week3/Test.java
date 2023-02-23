29770e33052b212ce2eee5cdffad9945ec78087b
leeyjwinter <james_22@naver.com>
2023-02-23 17:57:41 +0900

    new: 0223 code problem complete

--- src/main/java/week3/Test.java
+++ src/main/java/week3/Test.java
@@ -11,6 +11,7 @@ public class Test {
 	public static void main(String[] args) {
 		TreeMap<Integer, String> tMap = new TreeMap<Integer, String>();
 		Map<Integer, String> map = new HashMap<Integer, String>();
+<<<<<<< HEAD
 		Deque<Integer> deq = new ArrayDeque<>();
 //		tMap.put(5, "fff");
 //		tMap.put(3, "bbb");
@@ -30,6 +31,21 @@ public class Test {
 //		StringBuffer sb = new StringBuffer("abc");
 //		String rev = sb.reverse().toString();
 //		System.out.println(rev);
+=======
+		
+		tMap.put(5, "fff");
+		tMap.put(3, "bbb");
+		tMap.put(2, "ccc");
+		tMap.put(7, "ddd");
+		tMap.put(1, "aaa");
+		tMap.put(1, "cccc");
+		
+		String a = tMap.replace(2, "ddd");
+		System.out.println(a);
+		System.out.println(tMap);
+		System.out.println(tMap.firstEntry());
+		System.out.println(tMap.ceilingEntry(6)); //6 이사큰 키 중 가장 작은 키
+>>>>>>> parent of 7d75b6f (0222 : Practice Brute Force)
 	}
 
 }
