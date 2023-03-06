42e5eef711902db77979059ccc69ae5ae4fd1817
leeyjwinter <james_22@naver.com>
2023-03-06 22:49:54 +0900

    no message

--- src/main/java/week5/BOJ16236_아기상어.java
+++ src/main/java/week5/BOJ16236_아기상어.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.List;
