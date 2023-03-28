c30639173406bb8d184e711b49cf435e50b4e314
Lee Young Jun <86416273+leeyjwinter@users.noreply.github.com>
2023-03-28 17:58:36 +0900

    Add files via upload
    0328 : Upload Files

---
+++ src/week8/SWEA1259_금속막대v2.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class SWEA1259_금속막대v2 {
+
+	static int T;
+	static int N;
+	static int[] front;
+	static int[] rear;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t < T + 1; t++) {
+			N = Integer.parseInt(br.readLine());
+			front = new int[N + 1];
+			rear = new int[N + 1];
+
+			st = new StringTokenizer(br.readLine());
+			for (int i = 1; i <= N; i++) {
+				front[i] = Integer.parseInt(st.nextToken());
+				rear[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			for (int i = 1; i <= N; i++) {
+				dfs(rear[i]);
+			}
+		}
+	}
+
+	private static void dfs(int rear) {
+	}
+}
