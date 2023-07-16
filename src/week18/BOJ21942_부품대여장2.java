66b31f73932e1e9407778d7d2d3d7049bba139be
leeyjwinter <james_22@naver.com>
2023-07-16 12:50:47 +0900

    0715 : 부품대여장 (date)

---
+++ src/week18/BOJ21942_부품대여장2.java
@@ -0,0 +1,13 @@
+package week18;
+
+import java.text.SimpleDateFormat;
+
+public class BOJ21942_부품대여장2 {
+	
+	static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
